93956855806dfbb1b5f8e79b98c0d3f7460697be
Zavista <84294353+Zavista@users.noreply.github.com>
2026-03-31 22:52:42 -0400

    Add last_fetched_at to feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -6,3 +6,15 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT * from feeds;
+
+-- name: GetNextFeedToFetch :one
+SELECT * from feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+where id = $1
+RETURNING *;
